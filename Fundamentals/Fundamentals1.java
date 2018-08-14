70dc539747b0c27ec48bd05b4bae148df8fa3dde
MinJu <minju2655@cmis.ac.th>
2018-08-14 09:34:14 +0700

    stared on datatypes problem

---
+++ Fundamentals/Fundamentals1.java
@@ -0,0 +1,10 @@
+public class Fundamentals1
+{
+    public static void main ( String args[] ){
+        dataTypes();
+    } // end method main
+    public static void dataTypes(){
+        System.out.println();
+        //code the problems here
+    }
+} // end class Fundamentals1 
