9b0f5fb31631b75e91fd7c879836017e01307ec2
MinJu <minju2655@cmis.ac.th>
2018-09-19 11:54:30 +0700

    started on arrays

---
+++ HelloWorld/Arrays.java
@@ -0,0 +1,15 @@
+public class Arrays
+{
+    public static void main ( String args[] ){
+        int[] myList = {1, 2, 3, 4, 5};
+        double[] myList2 = {1.1, 2.2, 3.3, 4.4, 5.5};
+        String[] myList3 = {"one", "two", "three", "four", "five"};
+        System.out.println(myList[0]);
+        System.out.println(myList2[2]);
+        System.out.println(myList3[4]);
+    } // end method main
+    public static void printArray(int[] array, boolean skip){
+        int[] myList = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        
+    }
+} // end class Arrays 
