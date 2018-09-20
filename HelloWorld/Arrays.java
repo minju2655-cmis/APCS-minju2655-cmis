ef67797e0e18c4665d9348a3996bc1525408f822
MinJu <minju2655@cmis.ac.th>
2018-09-20 14:36:04 +0700

    fundamentals2

--- HelloWorld/Arrays.java
+++ HelloWorld/Arrays.java
@@ -4,12 +4,29 @@ public class Arrays
         int[] myList = {1, 2, 3, 4, 5};
         double[] myList2 = {1.1, 2.2, 3.3, 4.4, 5.5};
         String[] myList3 = {"one", "two", "three", "four", "five"};
-        System.out.println(myList[0]);
-        System.out.println(myList2[2]);
-        System.out.println(myList3[4]);
+        System.out.print(myList[0]+" / ");
+        System.out.print(myList2[2]+" / ");
+        System.out.print(myList3[4]+" ");
+        
+        System.out.print("\n");
     } // end method main
     public static void printArray(int[] array, boolean skip){
-        int[] myList = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-        
+        int[] myList ={1,2,3,4,5,6,7,8,9,10,11,12,13};
+        for (int ctr=0;ctr<myList.length;ctr++){
+            if (skip==true){
+            }
+        }
+    }
+    public static void printArray(double[] array, boolean skip){
+    
+    
+    }
+    public static void printArray(boolean[] array, boolean skip){
+    
+    
+    }
+    public static void printArray(String[] array, boolean skip){
+    
+    
     }
 } // end class Arrays 
