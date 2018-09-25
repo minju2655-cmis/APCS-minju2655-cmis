42af98fbe8b4fe88cb7a93ae669499162df50693
MinJu <minju2655@cmis.ac.th>
2018-09-25 11:55:24 +0700

    Fundamentals II

--- HelloWorld/Arrays.java
+++ HelloWorld/Arrays.java
@@ -5,9 +5,10 @@ public class Arrays
         double[] myList2 = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9};
         boolean[] myList3 ={true, false, false, true, true, false, false};
         String[] myList4 = {"one", "two", "three", "four", "five", "six", "seven", "eight"};
-        System.out.print(myList[0]+" / ");
-        System.out.print(myList2[2]+" / ");
-        System.out.print(myList4[4]+" ");
+        System.out.format("myList[0]: %d\nmyList2[2]: %g\nmyList[4] %s\n", myList[0],myList2[2],myList4[4]);
+        //System.out.print(myList[0]+" / ");
+        //System.out.print(myList2[2]+" / ");
+        //System.out.print(myList4[4]+" ");
 
         System.out.print("\n");
         printArray(myList,true);
@@ -19,7 +20,6 @@ public class Arrays
         printArray(myList4,true);
         System.out.print("\n");
         
-        lastItem(myList);
     } // end method main
     public static void printArray(int[] array, boolean skip){
         if (skip==true){
@@ -78,10 +78,31 @@ public class Arrays
         }   
     }
     public static int lastItem(int[] array){
-        int a=0;
-        for (int ctr=0;ctr<array.length;ctr++){
-            a+=array.length-1;
-        }
-        return a;
+        return array[array.length-1];
+    }
+    public static double lastItem(double[] array){
+        return array[array.length-1];
+    }
+    public static boolean lastItem(boolean[] array){
+        return array[array.length-1];
+    }
+    public static String lastItem(String[] array){
+        return array[array.length-1];
+    }
+    public static int middleItem(int[] array){
+        return array[array.length/2];
+    }
+    public static double middleItem(double[] array){
+        return array[array.length/2];
+    }
+    public static boolean middleItem(boolean[] array){
+        return array[array.length/2];
+    }
+    public static String middleItem(String[] array){
+        return array[array.length/2];
+    }
+    public static int[] randomInts(int n, int min, int max){
+        int[] array= new int[n];
+        return array;
     }
 } //end class Arrays 
