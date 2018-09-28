dc29f7d973bb3932e6eaf06939157cd537b8c155
MinJu <minju2655@cmis.ac.th>
2018-09-28 09:55:43 +0700

    FundamentalsII

--- HelloWorld/FundamentalsII.java
+++ HelloWorld/FundamentalsII.java
@@ -1,4 +1,5 @@
-public class Arrays
+import java.util.Arrays;
+public class FundamentalsII
 {
     public static void main ( String args[] ){
         int[] myList = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -26,7 +27,20 @@ public class Arrays
         System.out.println("The middle item is: "+ middleItem(myList4));
         
         System.out.print("\n");
-
+        System.out.println(Arrays.toString(randomInts(10, 10, 1)));
+        System.out.println(Arrays.toString(randomDoubles(10, 10, 1)));
+        
+        System.out.print("\n");
+        System.out.println(Arrays.toString(copy(myList)));
+        System.out.print("\n");
+        System.out.println(Arrays.toString(copy(myList2)));
+        System.out.print("\n");
+        System.out.println(Arrays.toString(copy(myList3)));
+        System.out.print("\n");
+        System.out.println(Arrays.toString(copy(myList4)));     
+        
+        System.out.print("\n");
+        System.out.println(Arrays.toString(pairs(8)));
     } // end method main
     public static void printArray(int[] array, boolean skip){
         if (skip==true){
@@ -109,17 +123,60 @@ public class Arrays
         return array[array.length/2];
     }
     public static int[] randomInts(int n, int min, int max){
-        int[] array= new int[n];
+        int[] random= new int[n];
         for (int ctr=0; ctr<n;ctr++){
-            array[ctr]=(int)(Math.random()*max-min)+min;
+            random[ctr]=(int)(Math.random()*(max-min))+min;
         }
-        return array;
+        return random;
     }
     public static double[] randomDoubles(int n, double min, double max){
-        double[] array= new double[n];
+        double[] random= new double[n];
         for (int ctr=0; ctr<n;ctr++){
-            array[ctr]=(Math.random()*max-min)+min;
+            random[ctr]=(Math.random()*(max-min))+min;
         }
-        return array;
+        return random;
     }
-} //end class Arrays 
+    public static int[] copy(int[] array){
+        int[] copy = new int[array.length];
+        System.out.println("Original: "+Arrays.toString(array));
+        System.out.print("Copy: ");
+        for (int ctr=0;ctr<array.length;ctr++){
+            copy[ctr]=array[ctr];
+        }
+        return copy;
+    }
+    public static double[] copy(double[] array){
+        double[] copy = new double[array.length];
+        System.out.println("Original: "+Arrays.toString(array));
+        System.out.print("Copy: ");
+        for (int ctr=0;ctr<array.length;ctr++){
+            copy[ctr]=array[ctr];
+        }
+        return copy;
+    }
+    public static boolean[] copy(boolean[] array){
+        boolean[] copy = new boolean[array.length];
+        System.out.println("Original: "+Arrays.toString(array));
+        System.out.print("Copy: ");
+        for (int ctr=0;ctr<array.length;ctr++){
+            copy[ctr]=array[ctr];
+        }
+        return copy;
+    }
+    public static String[] copy(String[] array){
+        String[] copy = new String[array.length];
+        System.out.println("Original: "+Arrays.toString(array));
+        System.out.print("Copy: ");
+        for (int ctr=0;ctr<array.length;ctr++){
+            copy[ctr]=array[ctr];
+        }
+        return copy;
+    }
+    public static int[] pairs(int n){
+        int[] pairs = new int[n];
+        for (int ctr=0;ctr<n;ctr++){
+           pairs[ctr]+=2;
+        }
+        return pairs;
+    }
+} //end class FundamentalsII 
