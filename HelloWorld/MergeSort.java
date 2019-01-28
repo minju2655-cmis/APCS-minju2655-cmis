3ab5986407b857d89badc1a89b6f7ed0fb308c8d
MinJu <minju2655@cmis.ac.th>
2019-01-28 11:54:35 +0700

    Still Struggling With (Life) Merge

--- HelloWorld/MergeSort.java
+++ HelloWorld/MergeSort.java
@@ -16,6 +16,61 @@ public class MergeSort{
         System.out.println();
     }
 
+    public static int[] Split(int[] array, boolean bool) {
+        int[] half;
+        if(array.length % 2 != 0 && bool == false){ 
+            half = new int[(array.length/2) + 1];
+        }
+        else{
+            half = new int[(array.length/2)];
+        }
+        if(bool == true){
+            for (int i = 0; i < (array.length/2); i++) {
+                half[i] = array[i];
+            }
+        }
+        else if (bool == false){
+            int x = array.length/2;
+            if(array.length % 2 != 0){
+                for (int i = 0; i < (array.length/2) + 1; i++) {
+                    half[i] = array[x];
+                    x++;
+                }
+            }
+            else{
+                for (int i = 0; i < array.length/2; i++) {
+                    half[i] = array[x];
+                    x++;
+                }
+            }
+        }
+        return half;
+    }
+
+    public static int[] Merge(int[] arr1, int[] arr2) {
+        int[] sorted = new int[arr1.length + arr2.length];
+        int x = 0;
+        int y = 0;
+        int xy=0;
+        int a=arr1.length;
+        int b=arr2.length;
+        for (int i = 0; i < sorted.length; i++) {     
+            if (arr1[x] <= arr2[y]) {
+                sorted[i] = arr1[x];
+                x++;
+                sorted[xy]=arr1[x];;
+                xy++;
+            }
+            else  {
+                sorted[i] = arr2[y];
+                y++;
+                sorted[xy]=arr2[y];;
+                xy++;
+            }
+        }
+        return sorted;
+    }
+
     public static int[] MergeSort(int[] array){
         if (array.length==1 || array.length==0){
             return array;
@@ -31,58 +86,6 @@ public class MergeSort{
         }
         return array;
     }
-
-    public static int[] Split(int[] array, boolean bool) {
-        int[] half;
-        if(array.length % 2 != 0 && bool == false){ 
-            half = new int[(array.length/2) + 1];
-        }
-        else{
-            half = new int[(array.length/2)];
-        }
-        if(bool == true){
-            for (int i = 0; i < (array.length/2); i++) {
-                half[i] = array[i];
-            }
-        }
-        else if (bool == false){
-            int i2 = array.length/2;
-            if(array.length % 2 != 0){
-                for (int i = 0; i < (array.length/2) + 1; i++) {
-                    half[i] = array[i2];
-                    i2++;
-                }
-            }
-            else{
-                for (int i = 0; i < array.length/2; i++) {
-                    half[i] = array[i2];
-                    i2++;
-                }
-            }
-        }
-        return half;
-    }
-    public static int[] Merge(int[] arr1, int[] arr2) {
-        int[] sorted = new int[arr1.length + arr2.length];
-        int x = 0;
-        int y = 0;
-        for (int i = 0; i < sorted.length; i++) {
-            if(arr1[x]==arr2[y]){
-                sorted[i]=arr1[x];
-            }
-            else{
-                if(arr1[x]<arr2[y]){
-                    sorted[i]=arr1[x];
-                    x++;
-                }
-                else if (arr1[x]>arr2[y]){
-                    sorted[i]=arr2[y];
-                    y++;
-                }
-            }
-        }
-        return sorted;
-    }
-   
+    
 }
 
