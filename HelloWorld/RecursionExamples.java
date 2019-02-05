823dee88b46cfac54c4e9f31a14c95f943bd54e4
MinJu <MinJu@192.168.0.5>
2019-02-06 02:29:08 +0700

    recursion

--- HelloWorld/RecursionExamples.java
+++ HelloWorld/RecursionExamples.java
@@ -3,13 +3,16 @@ public class RecursionExamples{
         method(-20);
         System.out.println("\n"+multiply(4,3));
         System.out.println("\n"+divide(9,3));
+        System.out.println("\n"+power(2,5));
     }
+
     public static void method(int arg){
         System.out.println(arg);
         if (arg!=0){
             method(arg+1);
         }
     }
+
     public static int multiply(int a, int b){
         if (b==1){
             return a;
@@ -21,6 +24,7 @@ public class RecursionExamples{
             return a + multiply(a,b-1);
         }
     }
+
     public static int divide(int a, int b){
         if (a==0){
             return a;
@@ -29,7 +33,29 @@ public class RecursionExamples{
             return a;
         }
         else{
-            return a - divide(a - 1, b);
+            if ((a > 0 && b > 0) || (a < 0 && b < 0)){
+                a -= b;
+                int quotient = 1 + divide(a, b);
+                return quotient;
+            }else{
+                a += b;
+                int quotient = -1 + divide(a, b);
+                return quotient;
+            }
         }
+    }
+    public static int power(int a, int b){
+        if (b == 0){
+            return 1;
+        }
+
+        else if (b == 1){
+            return a;
+        }
+
+        else{
+            return multiply(a, power(a, b - 1));
+        }
+
     }
 }
