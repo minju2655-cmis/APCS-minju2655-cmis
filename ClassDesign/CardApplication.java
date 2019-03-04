69d7e263448f931ed094e1e50de18bac7b6ea964
MinJu <minju2655@cmis.ac.th>
2019-03-04 14:33:42 +0700

    Card

---
+++ ClassDesign/CardApplication.java
@@ -0,0 +1,10 @@
+public class CardApplication
+{
+    public static void main(String[] args){
+        Card c = new Card();
+        System.out.print(c);
+        Card[] deck = new Card [52];
+        
+        System.out.print(deck);
+    }
+}
