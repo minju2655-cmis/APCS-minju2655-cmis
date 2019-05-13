e20d07759a26861ccdf5ff4beb1c870082c71076
MinJu <minju2655@cmis.ac.th>
2019-05-13 14:34:46 +0700

    Game

---
+++ Greenfoot/Game/MyWorld.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Rocket rocket = new Rocket();
+        addObject(rocket,301,186);
+        PPL1 ppl1 = new PPL1();
+        addObject(ppl1,349,262);
+        ppl1.setLocation(440,309);
+    }
+}
---
+++ Greenfoot/Game/Rocket.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Rocket here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Rocket extends Actor
+{
+    /**
+     * Act - do whatever the Rocket wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        move();
+    }    
+    public void move(){
+        if (Greenfoot.isKeyDown("left")){
+            move(-4);
+        }
+        if (Greenfoot.isKeyDown("right")){
+            move(4);
+            
+        }
+    }
+}
